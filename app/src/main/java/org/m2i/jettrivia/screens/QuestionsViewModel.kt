4f8327f8b5673ac62bc7d33bb404d04066b06f56
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-09-11 11:29:35 +0100

    add ViewModel

---
+++ app/src/main/java/org/m2i/jettrivia/screens/QuestionsViewModel.kt
@@ -0,0 +1,31 @@
+package org.m2i.jettrivia.screens
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import org.m2i.jettrivia.data.DataOrException
+import org.m2i.jettrivia.model.QuestionItem
+import org.m2i.jettrivia.repository.QuestionRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class QuestionsViewModel @Inject constructor(private val repository: QuestionRepository): ViewModel() {
+    val data : MutableState<DataOrException<ArrayList<QuestionItem>, Boolean, Exception>> =
+        mutableStateOf(DataOrException(null,true,Exception("")))
+
+    init {
+        getAllQuestions()
+    }
+    private fun getAllQuestions(){
+        viewModelScope.launch {
+            data.value.loading = true
+            data.value = repository.getAllQuestions()
+            if(data.value.data.toString().isNotEmpty())
+                data.value.loading = false
+        }
+    }
+
+}
